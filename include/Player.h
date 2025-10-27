68bc23a98ee2aaa7be83ff52c3553081e8d96d59
Alan <alan.gallo@student.uibk.ac.at>
2025-10-27 17:02:08 +0100

    pfusch

--- include/Player.h
+++ include/Player.h
@@ -8,8 +8,24 @@ private:
     bool isOnGround = false;
     float gravity = 800.f;
     float speed = 400.f;
+    float health = 10;
 
 public:
+    Player(sf::Vector2f size, sf::Vector2f position)
+        :  {}
+
+    float getIsOnGround() const { return isOnGround; }
+    void setIsOnGround(bool isOnGround) { this->isOnGround = isOnGround; }
+
+    float getGravity() const { return gravity; }
+    void setGravity(float gravity) { this->gravity = gravity; }
+
+    float getSpeed() const { return speed; }
+    void setSpeed(float gravity) { this->speed = speed; }
+
+    float getHealth() const { return health; }
+    void setHealth(float health) { this->health = health; }
+
     Player(float width, float height, float x, float y);
     sf::FloatRect getBounds() const override;
     void update(float dt) override;
