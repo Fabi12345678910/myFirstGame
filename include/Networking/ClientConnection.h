58684303c22977dc12c2acc999c841c738f69bf9
Fabian Pfaff <github@fabi00.de>
2025-11-15 16:34:35 +0100

    remade Connection.h and ClientConnection.h

--- include/Networking/ClientConnection.h
+++ include/Networking/ClientConnection.h
@@ -6,13 +6,21 @@ class ClientConnection : public Connection
 {
 private:
     /* data */
-public:
-    ClientConnection(sf::IpAddress target, void* args = NULL): Connection(std::make_unique<sf::TcpSocket>(), args){
+    ClientConnection(std::unique_ptr<sf::TcpSocket> tcpSocket, std::unique_ptr<sf::UdpSocket> udpSocket, sf::IpAddress udpReceipientIpAdress, unsigned short udpRecipientPort, void* eventHandlerArgs = NULL)
+        : Connection(std::move(tcpSocket), std::move(udpSocket), udpReceipientIpAdress, udpRecipientPort, eventHandlerArgs){
+    }
 
-        sf::Socket::Status status = socket->connect(target, 42069);
-        if (status != sf::Socket::Status::Done)
+public:
+    static ClientConnection createClientConnection(sf::IpAddress target, sf::IpAddress udpReceipientIpAdress, unsigned short udpRecipientPort, void* eventHandlerArgs = NULL){
+        std::unique_ptr<sf::TcpSocket> tcpSocket = std::make_unique<sf::TcpSocket>();
+        if (tcpSocket->connect(target, 42069) != sf::Socket::Status::Done)
         {
-            throw std::runtime_error("Error connecting to server");
+            throw std::runtime_error("Error connecting to tcp server");
         }
+        std::unique_ptr<sf::UdpSocket> udpSocket = std::make_unique<sf::UdpSocket>();
+        if(udpSocket->bind(sf::Socket::AnyPort) != sf::Socket::Status::Done){
+            throw std::runtime_error("Error binding local udp port");
+        }
+        return ClientConnection(std::move(tcpSocket), std::move(udpSocket), udpReceipientIpAdress, udpRecipientPort, eventHandlerArgs);
     }
 };
--- include/Networking/Connection.h
+++ include/Networking/Connection.h
@@ -7,26 +7,32 @@
 
 class Connection
 {
-protected:
-    std::unique_ptr<sf::TcpSocket> socket;
-
+private:
+    std::unique_ptr<sf::UdpSocket> udpSocket;
+    sf::IpAddress udpRecipientAdress;
+    unsigned short udpRecipientPort;
+    std::unique_ptr<sf::TcpSocket> tcpSocket;
+    void* eventHandlerArgs;
 public:
     void*(*eventHandler)(std::unique_ptr<Event>, Connection&, void* args) = NULL;
+    unsigned short getUdpPort(){return udpSocket->getLocalPort();}
     pthread_t eventHandlerThread;
     std::unique_ptr<Event> receiveNextEvent();
     //additional arguments passed into the event handler
-    Connection(std::unique_ptr<sf::TcpSocket> ptr, void* args = NULL){
-        this->socket = std::move(ptr);
-        this->args = args;
+    Connection(std::unique_ptr<sf::TcpSocket> tcpSocket, std::unique_ptr<sf::UdpSocket> udpSocket, sf::IpAddress udpReceipientIpAdress, unsigned short udpRecipientPort, void* eventHandlerArgs = NULL)
+    : udpRecipientAdress(udpReceipientIpAdress)
+    ,udpRecipientPort(udpRecipientPort)
+    ,eventHandlerArgs(eventHandlerArgs){
+        this->tcpSocket = std::move(tcpSocket);
+        this->udpSocket = std::move(udpSocket);
     };
     ~Connection() = default;
 
-    void* args = NULL;
     virtual void setArgs(void* args){
-        this->args = args;
+        this->eventHandlerArgs = args;
     }
     void* getArgs(){
-        return this->args;
+        return this->eventHandlerArgs;
     };
     void sendEvent(const Event& ev);
     void setEventHandler(void* handleEvent(std::unique_ptr<Event>, Connection&, void* args));
