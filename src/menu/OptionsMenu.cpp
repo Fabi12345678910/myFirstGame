390178c6887abfb8084083265b68f2df79279a2c
Alan <alan.gallo@student.uibk.ac.at>
2026-01-03 15:01:09 +0100

    please pipeline im begging u

--- src/menu/OptionsMenu.cpp
+++ src/menu/OptionsMenu.cpp
@@ -2,6 +2,7 @@
 #include "Options.h"
 #include <iostream>
 #include <sstream>
+#include <algorithm>
 
 OptionsMenu::OptionsMenu(sf::RenderWindow& win, Options& options)
     : window(win), opts(options)/*
