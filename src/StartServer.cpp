d6444603081ee0c62892dcef8a60f0f9a8b7a9a3
Fabian Pfaff <github@fabi00.de>
2026-01-07 20:05:14 +0100

    used port on server

--- src/StartServer.cpp
+++ src/StartServer.cpp
@@ -1,7 +1,19 @@
 #include "Server.h"
+#include <limits.h>
 int main(int argc, char const *argv[])
 {
-    Server server = Server();
+    unsigned short port = 4444;
+    if(argc > 1){
+        //parsing 
+        int iPort = atoi(argv[1]);
+        if (iPort > USHRT_MAX || iPort <= 0){
+            fprintf(stderr, "Invalid Port");
+            return -1;
+        }else{
+            port = iPort;
+        }
+    }
+    Server server = Server(port);
     server.run();
     return 0;
 }
