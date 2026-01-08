8f88a0643cf0bea526dccace9bc1e6b2fb175ef8
Fabian Pfaff <github@fabi00.de>
2026-01-08 17:13:39 +0100

    fixaas

--- include/Networking/EventDefinitions/EventLoginRequest.h
+++ include/Networking/EventDefinitions/EventLoginRequest.h
@@ -1,21 +1,24 @@
 #pragma once
 #include "Networking/Event.h"
 #include "Networking/EventTypeList.h"
+#include "Config.h"
 
 class EventLoginRequest:public Event
 {
 public:
     unsigned short udpPort;
+    VERSION_TYPE apiVersion;
     EventLoginRequest(sf::Packet packet){
-        if(!(packet >> udpPort)){
-            throw std::runtime_error("failed to read udp port");
+        if(!(packet >> apiVersion >> udpPort)){
+            throw std::runtime_error("failed to read packet contents");
         };
     };
-    EventLoginRequest(unsigned short udpPort): udpPort(udpPort){
+    EventLoginRequest(unsigned short udpPort, VERSION_TYPE apiVersion = API_VERSION): udpPort(udpPort), apiVersion(apiVersion){
     }
     sf::Packet toPacket() const override{
         sf::Packet packet;
         packet << (DATATYPE_EVENT_TYPE) EVENT_TYPE_LOGIN_REQUEST;
+        packet << apiVersion;
         packet << udpPort;
         return packet;
     }
